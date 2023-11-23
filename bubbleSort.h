da0d5e283feae051ac1cead8f17f2cba0e6aad78
MarioMonts <mario.montes@uniat.edu.mx>
2023-11-23 12:08:25 -0600

    Mario Montes Updates

--- bubbleSort.h
+++ bubbleSort.h
@@ -3,14 +3,14 @@
 
 #include <vector>
 template<typename T>
-void bubbleSort(std::vector<T>& arr){
-  auto n = arr.size();
-  for (unsigned long i = 0; i < n-1; ++i) {
-    for (unsigned long j = 0; j < n-i-1; ++j) {
-      if (arr[j] > arr[j + 1]) {
-        T temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+void bubbleSort(std::vector<T>& arr){ // La función bubblesort toma un vector T por referencia
+  auto n = arr.size(); // Esto es para saber cuál es el tamaño del vector que se va a ordenar
+  for (unsigned long i = 0; i < n-1; ++i) { // Es un ciclo para recorrer todos los elementos del vector
+    for (unsigned long j = 0; j < n-i-1; ++j) { // Este es otro ciclo que va a comparar y ordenar los elementos del vector
+      if (arr[j] > arr[j + 1]) { // Esta parte compara los elementos que son adyacentes
+        T temp = arr[j]; // Esta parte es el intercambio de elementos utilizando una variable temporal
+        arr[j] = arr[j + 1]; // Esta parte es el intercambio de elementos utilizando una variable temporal
+        arr[j + 1] = temp; // Esta parte es el intercambio de elementos utilizando una variable temporal
       }
     }
   }
