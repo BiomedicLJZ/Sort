1ec2b74786d99da3d2bfe21ec3817cde32ee4b02
DavidElPapu <david.jung@uniat.edu.mx>
2023-11-27 03:40:22 -0600

    Le hice un cambio muy pequeño al insertionSort, pero almenos ya corre un poco mas rapido xd, si es demasiado poco el cambio y no vale digame y veo que mas puedo cambiarle, pero si estuvo dificil porque casi todo lo que vi me parecio inmejorable

--- insertionSort.h
+++ insertionSort.h
@@ -13,11 +13,12 @@ void insertionSort(std::vector<Comparable>& a) {
   for (size_t p = 1; p < a.size(); ++p) {
     // Use std::move for efficiency when dealing with large objects
     Comparable tmp = std::move(a[p]);
-    size_t j;
+    size_t j = p;
 
     // Find the correct position for the current element in the sorted part of the array
-    for(j = p; j > 0 && tmp < a[j - 1]; --j) {
-      a[j] = std::move(a[j - 1]);
+    while(j > 0 && tmp <a[j - 1]) {
+        a[j] = std::move(a[j - 1]);
+        j--;
     }
 
     // Place the current element in the correct position
@@ -25,3 +26,6 @@ void insertionSort(std::vector<Comparable>& a) {
   }
 }
 #endif // INSERTIONSORT_H
+
+//Los cambios que hice fue nomas cambiar los for a while, ya que los for son muy pesados
+//La razon por la que deje el primer for es que por alguna razon, al convertirlo a while tardaba mas que dejandolo como for
