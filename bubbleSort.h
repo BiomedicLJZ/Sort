ed2258248e42c82acf8b33ded85edb40528e5501
Ketosvaldo <ketchulo.conk2013@gmail.com>
2023-11-28 08:51:47 -0600

    Better bubble sort

--- bubbleSort.h
+++ bubbleSort.h
@@ -5,13 +5,21 @@
 template<typename T>
 void bubbleSort(std::vector<T>& arr){
   auto n = arr.size();
+  bool switched = false;
   for (unsigned long i = 0; i < n-1; ++i) {
-    for (unsigned long j = 0; j < n-i-1; ++j) {
+      switched = false;
+    for (unsigned long j = 0; j < n-i-1; ++j)
+    {
       if (arr[j] > arr[j + 1]) {
         T temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        switched = true;
       }
+    }
+    if (!switched)
+    {
+        break;
     }
   }
 }
