670c6bdc308287559604005a1d5bd632c96be65f
LuigiTan <gibran.garcia@uniat.edu.mx>
2023-11-28 13:30:03 -0600

    Bubble Comments

--- bubbleSort.h
+++ bubbleSort.h
@@ -4,12 +4,12 @@
 #include <vector>
 template<typename T>//Maybe maybe
 void bubbleSort(std::vector<T>& arr){
-  auto n = arr.size();
+  auto n = arr.size(); //Se iguala una nueva variable llamada n a el tamaño del arreglo
   for (unsigned long i = 0; i < n-1; ++i) {
     for (unsigned long j = 0; j < n-i-1; ++j) {
       if (arr[j] > arr[j + 1]) {
-        T temp = arr[j];
-        arr[j] = arr[j + 1];
+        T temp = arr[j];//Se guarda el elemento en una variable temporal
+        arr[j] = arr[j + 1];//Se guarda en el espacio anterior a el elemento siguiente
         arr[j + 1] = temp;
       }
     }
