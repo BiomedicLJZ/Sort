4cc8ae7894782b5153017314a36b20fa399727f9
TheBlaze <santi.volcanos@gmail.com>
2023-11-27 21:23:54 -0600

    comentario bubble

--- bubbleSort.h
+++ bubbleSort.h
@@ -3,16 +3,16 @@
 
 #include <vector>
 template<typename T>
-void bubbleSort(std::vector<T>& arr){
-  auto n = arr.size();
-  for (unsigned long i = 0; i < n-1; ++i) {
-    for (unsigned long j = 0; j < n-i-1; ++j) {
-      if (arr[j] > arr[j + 1]) {
+void bubbleSort(std::vector<T>& arr){//funcion de bubble sort que aprovecha el template para funcionar con cualquier tipo de dato
+  auto n = arr.size();//toma el tamano del vector,
+  for (unsigned long i = 0; i < n-1; ++i) {//for para el tamano de el arreglo tomando en cuenta que comienza en 0
+    for (unsigned long j = 0; j < n-i-1; ++j) {//for que va haciendose mas pequeno dependiendo de la cantidad
+      if (arr[j] > arr[j + 1]) {//compara los datos usando j y los cambia con la variable de paso temp si el primer termino es mayor al segundo
         T temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
       }
     }
-  }
+  }//bubble sort has an average of n^2 in time complexity and a 1 space complexity
 }
 #endif //BUBBLESORT_H
