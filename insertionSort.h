1e4b38df533b6b3ae5b43c68c835e99934be76b6
HyanJF <hyan.fierros@uniat.edu.mx>
2023-11-27 18:36:19 -0600

    insertionSort comentado

--- insertionSort.h
+++ insertionSort.h
@@ -4,23 +4,26 @@
 #include <vector>
 #include <iostream>
 #include <cstddef>
+// include añadimos nuestras bibliotecas para su uso posterior
 
-// This function implements the insertion sort algorithm to sort a vector of elements in ascending order.
-// It iterates over each element, finds the correct position for it in the sorted part of the vector, and places it there.
-template <typename Comparable>
+
+template <typename Comparable> //Utilizamos esta plantilla para definir que se utilizaran datos para comparar
 void insertionSort(std::vector<Comparable>& a) {
-  // Iterate over each element in the vector
+  // Funcion del metodo de ordenamiento (resive un vector)
   for (size_t p = 1; p < a.size(); ++p) {
-    // Use std::move for efficiency when dealing with large objects
+    // Utilizando un for para iterar el vector desde la posicion 1 en p
     Comparable tmp = std::move(a[p]);
-    size_t j;
+    // tmp guarda la informacion moviendola de variable
 
-    // Find the correct position for the current element in the sorted part of the array
+    // Se compara la informacion para dejarla en el mismo lugar o moverla de lado derecho en la posicion correcta
+    // Si es mayo entonces se movera a la derecha
+    size_t j;
     for(j = p; j > 0 && tmp < a[j - 1]; --j) {
       a[j] = std::move(a[j - 1]);
+
     }
 
-    // Place the current element in the correct position
+      // Se coloca el elemento en la posicion deseada
     a[j] = std::move(tmp);
   }
 }
